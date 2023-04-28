4f55cd616ffc8abb1a90b5916841640bb7e1a591
vadimboganov <vadimboganov@gmail.com>
2023-04-28 09:56:19 +0300

    add logger

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/VadimBoganov/fulgur
+
+go 1.20
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.15.2
+	github.com/sirupsen/logrus v1.9.0
+)
+
+require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.10 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
---
+++ pkg/logging/logger.go
@@ -0,0 +1,80 @@
+package logger
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"io"
+	"os"
+	"path"
+	"runtime"
+	"sync"
+)
+
+var once sync.Once
+var instance *logrus.Logger
+
+func GetLogger() *logrus.Logger{
+	once.Do(func(){
+		instance = New()
+	})
+	
+	return instance
+}
+
+type writeHook struct{
+	Writers []io.Writer
+	LogLevels []logrus.Level
+}
+
+func (hook *writeHook) Fire(entry *logrus.Entry) error{
+	line, err := entry.String()
+	if err != nil{
+		return err
+	}
+	
+	for _, w := range hook.Writers{
+		_, err := w.Write([]byte(line))
+		if err != nil {
+			return err
+		}
+	}
+	
+	return err
+}
+
+func (hook *writeHook) Levels() []logrus.Level{
+	return hook.LogLevels
+}
+
+func New() *logrus.Logger {
+	l := logrus.New()
+	l.SetReportCaller(true)
+	l.SetFormatter(&logrus.TextFormatter{
+		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
+			fileName := path.Base(frame.File)
+			return fmt.Sprintf("%s()", frame.Function), fmt.Sprintf("%s:%d", fileName, frame.Line)
+		},
+		DisableColors: false,
+		FullTimestamp: true,
+	})
+	
+	err := os.MkdirAll("logs", 0740)
+	if err != nil{
+		panic(err)
+	}
+	
+	file, err := os.OpenFile("logs/logs.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0640)
+	if err != nil{
+		panic(err)
+	}
+	
+	l.SetOutput(io.Discard)
+	
+	l.AddHook(&writeHook{
+		Writers: []io.Writer{file, os.Stdout},
+		LogLevels: logrus.AllLevels,
+	})
+	
+	l.SetLevel(logrus.TraceLevel)
+		return l
+}
