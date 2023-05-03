2f19a978b6ade75b226655fa9875f8063bc1b0a6
vadimboganov <vadimboganov@gmail.com>
2023-05-03 10:09:38 +0300

    first version

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,27 @@
 package main
 
-	println("Hello")
+import (
+	config2 "github.com/VadimBoganov/fulgur/internal/config"
+	db2 "github.com/VadimBoganov/fulgur/internal/db"
+	logger2 "github.com/VadimBoganov/fulgur/internal/logging"
+)
+
+func main(){
+	logger := logger2.GetLogger()
+
+	var config = &config2.Config{}
+
+	if err := config.InitConfig("internal/config"); err != nil {
+		logger.Error("Error occured while initialize config: %s", err.Error())
+	}
+
+	db, err := db2.NewDB(config.DatabasePath)
+	if db == nil{
+		logger.Error("Cant open db: %s", err.Error())
+	}
+
+	err = db2.RunMigrations(db)
+	if err != nil{
+		logger.Error(err)
+	}
 }
